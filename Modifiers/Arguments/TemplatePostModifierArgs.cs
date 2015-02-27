48d151676ce5f6a3f86b1ec8362740480f2e56d5
Andrew Jameson <andrew_jameson@hotmail.com>
2015-02-27 06:40:48 +0100

    Delete TemplatePostModifierArgs.cs

--- Modifiers/Arguments/TemplatePostModifierArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using SuperScript.Modifiers;
-
-namespace SuperScript.Templates.Modifiers.Arguments
-{
-	public class TemplatePostModifierArgs : PostModifierArgs
-	{
-		public string ScriptContents { get; set; }
-	}
-}
